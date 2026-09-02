ab34a22bf68c907d6b36f6572102fe56544296f8
TuNN <tu.nguyen1@savvycomsoftware.com>
2026-09-03 03:00:13 +0700

    Add ranking sections to novel extensions

--- xiaoshuo-tw/src/home.js
+++ xiaoshuo-tw/src/home.js
@@ -2,7 +2,10 @@ function execute() {
     return Response.success([
         { title: "最近更新", input: "/top/lastupdate/", script: "search.js" },
         { title: "最新入庫", input: "/top/postdate/", script: "search.js" },
-        { title: "熱門排行", input: "/top/allvisit/", script: "search.js" },
+        { title: "Top tháng", input: "/top/monthvisit/", script: "search.js" },
+        { title: "Top toàn bộ", input: "/top/allvisit/", script: "search.js" },
+        { title: "Đề xuất tháng", input: "/top/monthvote/", script: "search.js" },
+        { title: "Đề xuất toàn bộ", input: "/top/allvote/", script: "search.js" },
         { title: "完本小說", input: "/wanben/", script: "search.js" }
     ]);
 }
